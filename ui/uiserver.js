cb85ad577ff01fe74fbe2d2891fb95a9377c4975
mwong125 <mj.wong120@gmail.com>
2019-06-26 18:18:32 -0400

    chapter 10 i think

--- ui/uiserver.js
+++ ui/uiserver.js
@@ -1,5 +1,6 @@
 
 require('dotenv').config();
+const path = require('path');
 const express = require('express');
 
 const app = express();
@@ -27,6 +28,10 @@ const env = { UI_API_ENDPOINT };
 
 app.get('/env.js', (req, res) => {
   res.send(`window.ENV = ${JSON.stringify(env)}`);
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve('public/index.html'));
 });
 
 const port = process.env.UI_SERVER_PORT || 8000;
